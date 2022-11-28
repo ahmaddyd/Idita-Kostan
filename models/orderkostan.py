8d25be0311e3c36a0924429a8523e05da35751cd
ahmaddyd <ahmdyulian.86@gmail.com>
2022-11-29 01:45:10 +0700

    Fixing All Error

--- models/orderkostan.py
+++ models/orderkostan.py
@@ -9,12 +9,10 @@ class OrderHotel(models.Model):
     _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = 'Order Kamar Kostan'
 
-    name = fields.Many2one('res.partner', string='Nama Penyewa', required=True, domain="[('is_customernya','=',True)]")
-    tanggal_masuk_kostan = fields.Date(string='Tanggal Masuk Kostan', default=fields.Datetime.now, required=True)
-    tanggal_keluar_kostan = fields.Date(string='Tanggal Selesai Kostan', required=True)
-
-    detailorder_ids = fields.One2many('idita.detail_order_kostan', 'order_ids', string='Detail Order')
-
+    name = fields.Many2one('res.partner', string='Nama Penyewa Kostan', required=True, domain="[('penyewa','=',True)]")
+    tanggal_masuk_kostan = fields.Datetime(default=fields.Datetime.now, string='Tanggal Masuk Kostan')
+    tanggal_keluar_kostan = fields.Datetime(string='Tanggal Keluar Kostan', required=True)
+    detailorder_ids = fields.One2many('idita.detail_order_kostan', 'order_ids', string='DETAIL ORDER')
     jumlah_hari = fields.Integer(compute="_compute_jumlah_hari", string='Total Hari', readonly=True, store=True)
 
     @api.depends('tanggal_masuk_kostan', 'tanggal_keluar_kostan')
@@ -45,11 +43,11 @@ class OrderHotel(models.Model):
 
     state = fields.Selection([
         ('draft', 'Draft'),
-        ('confirm', 'Dikonfirmasi'),
-        ('pay', 'Menunggu Konfirmasi Pembayaran'),
+        ('confirm', 'Konfirmasi'),
+        ('pay', 'Menunggu Konfirmasi Pembayaran....'),
         ('done', 'Terkonfirmasi'),
-        ('not_confirm', 'Ditolak Pembayaran Anda'),
-        ('cancel', 'Dibatalkan'),
+        ('not_confirm', 'Ditolak'),
+        ('cancel', 'Batal'),
     ], string='States', default='draft', tracking=True)
 
     def action_draft(self):
@@ -58,25 +56,25 @@ class OrderHotel(models.Model):
     def action_confirm(self):
         self.state = 'confirm'
 
+    def action_pay(self):
+        self.state = 'pay'
+
     def action_done(self):
         self.state = 'done'
         code = True
         while code:
-            random_code = random.sample(range(1000, 1999), 1)
-            code_random = "CST" + str(random_code[0])
-            search_code = self.env['idita.order_kostan'].search([('kode_konfirmasi', '=', code_random)])
+            rand_code = random.sample(range(1000, 1999), 1)
+            mycode = "CUST" + str(rand_code[0])
+            search_code = self.env['idita.order_kostan'].search([('kode_konfirmasi', '=', mycode)])
             if len(search_code) < 1:
-                self.kode_konfirmasi = code_random
+                self.kode_konfirmasi = mycode
                 break
-
-    def action_cancel(self):
-        self.state = 'cancel'
-
-    def action_pay(self):
-        self.state = 'pay'
 
     def action_not_confirm(self):
         self.state = 'not_confirm'
+
+    def action_cancel(self):
+        self.state = 'cancel'
 
 
 class DetailOrder(models.Model):
@@ -84,18 +82,21 @@ class DetailOrder(models.Model):
     _description = 'Detail Order Kamar Kostan'
 
     name = fields.Char(string='Kode Order')
-    models_id = fields.Many2one('idita.kamar_kostan', string='Tipe Kamar Kostan')
-    order_ids = fields.Many2one(comodel_name='idita.order_kostan', string='Order Ruangan')
-    jumlah_kamar = fields.Integer(string='Jumlah Kamar', default=1)
+    tipe_kamar_id = fields.Many2one('idita.kamar_kostan', string='Tipe Kamar')
+    order_ids = fields.Many2one('idita.order_kostan', string='Order Kostan')
+
+    jumlah_kamar = fields.Integer(string='Jumlah Kamar Kostan', default=1)
+
     harga = fields.Integer(compute='_compute_harga_kamar', string='Harga per bulan')
+
+    @api.depends('tipe_kamar_id')
+    def _compute_harga_kamar(self):
+        for record in self:
+            record.harga = record.tipe_kamar_id.harga
+
     total_harga = fields.Integer(compute='_compute_total_harga', string='Total Harga')
 
     @api.depends('harga', 'jumlah_kamar')
     def _compute_total_harga(self):
         for record in self:
             record.total_harga = record.jumlah_kamar * record.harga
-
-    @api.depends('models_id')
-    def _compute_harga_kamar(self):
-        for record in self:
-            record.harga = record.models_id.harga
