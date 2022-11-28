8f8a7f029b6c297492853a9d402e285cf7cfe1d1
ahmaddyd <ahmdyulian.86@gmail.com>
2022-11-28 15:03:54 +0700

    Modifier Partner

--- models/partner.py
+++ models/partner.py
@@ -6,5 +6,5 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     is_pegawainya = fields.Boolean(string='Pegawai', default=False)
-    is_customernya = fields.Boolean(string='Customer', default=False)
+    is_customernya = fields.Boolean(string='Penyewa', default=False)
     Order_ids = fields.One2many(comodel_name='idita.kostan_order', inverse_name='name', string='Nama Order')
